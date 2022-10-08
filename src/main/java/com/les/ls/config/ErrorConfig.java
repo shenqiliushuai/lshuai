58fed4273b81f9bc82ff80022921065fc2b5c3f4
les.liu <les.liu@7road.com>
2022-10-08 10:51:17 +0800

    多环境配置

---
+++ src/main/java/com/les/ls/config/ErrorConfig.java
@@ -0,0 +1,19 @@
+package com.les.ls.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class ErrorConfig implements ErrorPageRegistrar {
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/error.html");
+        ErrorPage error400Page = new ErrorPage(HttpStatus.BAD_REQUEST, "/error.html");
+        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/error.html");
+        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error.html");
+        registry.addErrorPages(error400Page, error401Page, error404Page, error500Page);
+    }
+}
