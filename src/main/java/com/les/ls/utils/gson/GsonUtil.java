cffdd27e3591965a4609328b0f60b99af91e4899
les.liu <les.liu@7road.com>
2022-10-09 15:49:07 +0800

    添加gson

---
+++ src/main/java/com/les/ls/utils/gson/GsonUtil.java
@@ -0,0 +1,45 @@
+package com.les.ls.utils.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.ToNumberPolicy;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.internal.bind.ObjectTypeAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Gson number转Double处理
+ */
+public class GsonUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(GsonUtil.class);
+
+    public static Gson getGson() {
+
+        Gson gson = new GsonBuilder().create();
+        try {
+            Field factories = Gson.class.getDeclaredField("factories");
+            factories.setAccessible(true);
+            Object o = factories.get(gson);
+            Class<?>[] declaredClasses = Collections.class.getDeclaredClasses();
+            for (Class<?> c : declaredClasses) {
+                if ("java.util.Collections$UnmodifiableList".equals(c.getName())) {
+                    Field listField = c.getDeclaredField("list");
+                    listField.setAccessible(true);
+                    List<TypeAdapterFactory> list = (List<TypeAdapterFactory>) listField.get(o);
+                    int i = list.indexOf(ObjectTypeAdapter.getFactory(ToNumberPolicy.DOUBLE));
+                    list.set(i, GsonTypeAdapter.FACTORY);
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Gson处理异常,error={}", e.getMessage(), e);
+        }
+        return gson;
+    }
+}
