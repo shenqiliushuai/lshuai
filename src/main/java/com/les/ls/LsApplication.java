bf15395b0a1a1bc9c17fe511e01ca31dd61a49cf
les.liu <les.liu@7road.com>
2022-10-27 15:49:20 +0800

    更改配置

--- src/main/java/com/les/ls/LsApplication.java
+++ src/main/java/com/les/ls/LsApplication.java
@@ -3,7 +3,6 @@ package com.les.ls;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
@@ -11,11 +10,12 @@ import javax.annotation.Resource;
 
 @MapperScan("com.les.ls.dao")
 @ComponentScan(basePackages = {"com.les.shengkai", "com.les.ls", "com.les.yjhui"})
-//@SpringBootApplication()
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class LsApplication {
 
-
+    /**
+     * 用于读取配置，优先级高于yml
+     */
     @Resource
     private Environment env;
 
